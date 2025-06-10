29542d63acdead1c82f969a236a690dd5a27e157
token <61819790+239573049@users.noreply.github.com>
2025-06-10 19:13:18 +0800

    新增优化image提示词的配置文件和文本模板，提升提示词生成的灵活性和效果，同时确保相关文件在输出目录中可用，增强功能调用的效果。

--- src/Console.Service/Services/PromptService.cs
+++ src/Console.Service/Services/PromptService.cs
@@ -178,6 +178,97 @@ public class PromptService(IDbContext dbContext, ILogger<PromptService> logger) 
         }
     }
 
+
+    [EndpointSummary("优化image提示词")]
+    public async Task OptimizeImagePromptAsync(
+        [FromBody] GeneratePromptInput input, HttpContext context)
+    {
+        var token = context.Request.Headers["Authorization"].ToString().Trim().Replace("Bearer ", "");
+
+        var apiUrl = context.Request.Headers["X-Api-Url"].ToString();
+
+        if (string.IsNullOrEmpty(token))
+        {
+            context.Response.StatusCode = 401;
+            throw new UnauthorizedAccessException("未授权访问，请提供有效的API令牌。");
+        }
+
+        if (string.IsNullOrEmpty(apiUrl) || apiUrl.Contains("/openai"))
+        {
+            apiUrl = ConsoleOptions.OpenAIEndpoint;
+        }
+
+        bool isFirst = true;
+
+        var kernelBuilder = Kernel.CreateBuilder()
+            .AddOpenAIChatCompletion(input.ChatModel, new Uri(apiUrl),
+                token);
+        var kernel = kernelBuilder.Build();
+        var plugins = kernel.CreatePluginFromPromptDirectory(
+            Path.Combine(AppContext.BaseDirectory, "plugins", "Generate"),
+            "Generate");
+
+        var result = new StringBuilder();
+
+        await foreach (var item in kernel.InvokeStreamingAsync(plugins["OptimizeImagePrompt"],
+                           new KernelArguments(
+                               new OpenAIPromptExecutionSettings()
+                               {
+                                   MaxTokens = 32000,
+                                   Temperature = 0.7f,
+                               })
+                           {
+                               { "prompt", input.Prompt },
+                               { "requirement", input.Requirements }
+                           }))
+        {
+            if (isFirst)
+            {
+                context.Response.Headers.ContentType = "text/event-stream";
+                context.Response.Headers.CacheControl = "no-cache";
+                context.Response.Headers.Connection = "keep-alive";
+
+                isFirst = false;
+            }
+
+            if (item is OpenAIStreamingChatMessageContent chatMessageContent)
+            {
+                if (chatMessageContent.Content == null)
+                {
+                    continue;
+                }
+
+                await context.Response.WriteAsync($"data: {JsonSerializer.Serialize(new
+                {
+                    message = chatMessageContent.Content,
+                    type = "message",
+                }, JsonSerializerOptions.Web)}\n\n");
+
+                result.Append(chatMessageContent.Content);
+
+                await context.Response.Body.FlushAsync();
+            }
+        }
+
+        await context.Response.WriteAsync("data: [DONE]\n\n");
+
+        await context.Response.Body.FlushAsync();
+
+        var entity = new PromptHistory()
+        {
+            Id = Guid.NewGuid(),
+            Requirement = input.Requirements ?? "",
+            Result = result.ToString(),
+            CreatedTime = DateTime.Now,
+            DeepReasoning = "",
+            Prompt = input.Prompt
+        };
+
+        await dbContext.PromptHistory.AddAsync(entity);
+
+        await dbContext.SaveChangesAsync();
+    }
+
     private async Task DeepReasoningFunctionCallingAsync(OptimizeFunctionCallingPromptInput input, HttpContext context,
         string token,
         string apiUrl)
